8cdcd20cbc563af7ffdef25a3951afadbf62c595
Nagesh-DevTools <nageshk@devtools.in>
2025-04-29 11:54:49 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -1,11 +1,19 @@
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQChAW4y8i5fIsV35zWQipTHVNe94PITWTeK9YnJ1LheUjabOM3HrkyHE67KymzMUxoHW/zURjZ2jEggB68YBV+nVnc+n/jKVzLBE/o8lDpLyWQ9vnx297FJ+h22x7Y9Fn1D/VpuW6xK7+kIU1tBNWmrDhkQfi8dNr8HPGP0gXD9iE+8srzwgapepbhHQFXYgtOeVuzlU9Wmo237MrDhXhEDzuKK8X8gL8zks8ZM16jRCwWgA8ak3oJpDoqwWInU9QS7T5eowJXOBNmV9IyXWxE/4zU8a2Ln2skjYHyLYorF3IO2WzeVnTnVTxYKatAHpeNwrT1pAsbzxEkSNHYbew0xbWjmv3siiQNPTZNXsXG++Qx9hBhidX9P57Ne1SQWL8xEQKhNwJBRflIE21LPDJ0YvxbNGYgJbTyhuimeF4AyAT0OY8R9veNMikiDjPH6kmGoRm+KBWeWUvF9aD2LawCoGR2FD0o8lIdDsRVudHuTpUiSV7D19kR/ytKf9o3Jx+0= AzureAD+VinodManjunatha@DT-VinodM
-
-
-85a3bd915a3a4ff81faccebdb5d8fca6
+# secrets.py - Don't commit this file to version control!
  
+# Example API keys, tokens, or credentials (for illustrative purposes only)
 
+API_KEY = "12345678-abcdefg-12345-6789abc"
 
-curl -u admin:"#Shravya@12345" -X GET "http://20.82.142.170:8082/artifactory/api/storage/sng-rpm-local/nginx-1.18.0-1.el7.ngx.x86_64.rpm?properties=test-environment-rpm-packages.environments=staging"
+SECRET_KEY = "9876543210hgfedc-abcdefg-12345"
+
+DATABASE_PASSWORD = "securepassword123"
+
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
  
-curl -u admin:"#Shravya@12345" -X GET "http://20.82.142.170:8082/artifactory/api/storage/sng-rpm-local/nginx-1.18.0-1.el7.ngx.x86_64.rpm?properties=test-environment-rpm-packages.environments"
+# Other sensitive data (like connection strings, OAuth tokens, etc.)
+
+OAUTH_TOKEN = "ya29.a0AfH6SMDGsC9GkDphdFpy5gD73Xj3Vjdwoiyc_olFxLmj8d3XjEjSHK7"
+
  
