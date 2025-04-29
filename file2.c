be307d13bd9f2010139c3ad0c45cbfe3ee79f5e3
Nagesh-DevTools <nageshk@devtools.in>
2025-04-29 12:15:20 +0530

    Update file2.c

--- file2.c
+++ file2.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-
-void vulnerable_function() {
-    char buffer[10];
-    printf("Enter some text: ");
-    gets(buffer);  // ❌ Dangerous function
-    printf("You entered: %s\n", buffer);
-}
 
 int main() {
-    vulnerable_function();1
+    printf("Hello, World!\n");
     return 0;
 }
-
