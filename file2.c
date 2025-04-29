b93d5c3e5ae0664427d6585da049e71383d4acb6
Nagesh-DevTools <nageshk@devtools.in>
2025-04-29 12:02:43 +0530

    Create file2.c

---
+++ file2.c
@@ -0,0 +1,14 @@
+import boto3
+
+# ❌ Hardcoded credentials (a security risk!)
+aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Using the keys to connect to AWS
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+print("Connected to S3!")
